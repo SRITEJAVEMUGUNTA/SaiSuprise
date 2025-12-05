4df186fb4664c9b51b6e5e762ff87c718fe9f064
Sriteja Vemugunta <svemugu@gmail.com>
2025-12-05 14:42:03 -0600

    Hello

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import image from "./assets/hello.png";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <img src={image} width={800} height={800} />
+    </>
+  );
+}
+
+export default App;
